9e26758e585069e3298e814bfbc8656645a0acf2
DidierOne <dpd5518@psu.edu>
2020-08-28 20:41:36 +0000

    made an function

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-temp_in_c = float(input("Enter temperature in celsius: "))
+  
+def cel_to_far():
+  temp_in_c = float(input("Enter temperature in celsius: "))
 
-#(0°C × 9/5) + 32 from Google
-temp_in_f = (temp_in_c *9/5) + 32
-temp_in_f = str(temp_in_f)
-temp_in_c = str(temp_in_c)
-print(temp_in_c + "° in Celsius is equivalent to " + temp_in_f + "° Fahrenheit.")
+  #(0°C × 9/5) + 32 from Google
+  temp_in_f = (temp_in_c *9/5) + 32
+  temp_in_f = str(temp_in_f)
+  temp_in_c = str(temp_in_c)
+  print(temp_in_c + "° in Celsius is equivalent to " + temp_in_f + "° Fahrenheit.")
+cel_to_far()
