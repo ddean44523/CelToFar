0fd04715d3c27d8dc419ad476bb2c873fcae5d57
DidierOne <dpd5518@psu.edu>
2020-08-28 20:25:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+temp_in_c = float(input("Enter temperature in celsius:"))
+
+#(0°C × 9/5) + 32 from Google
+temp_in_f = (temp_in_c *9/5) + 32
+temp_in_f = str(temp_in_f)
+temp_in_c = str(temp_in_c)
+print(temp_in_c + "° Celsius is equivelant to " + temp_in_f + " ° Fahrenheit.")
